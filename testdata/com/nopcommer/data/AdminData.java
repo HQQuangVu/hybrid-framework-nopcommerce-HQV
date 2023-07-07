bcec63878837edc62362e4f4e5f047fefc162ac0
HQQuangVu <hq.quangvu@gmail.com>
2023-07-07 08:23:16 +0700

    Init

---
+++ testdata/com/nopcommer/data/AdminData.java
@@ -0,0 +1,19 @@
+package com.nopcommer.data;
+
+public class AdminData {
+	public class Register {
+
+	}
+
+	public class Login {
+
+	}
+
+	public class Address {
+
+	}
+
+	public class MyAccount {
+
+	}
+}
